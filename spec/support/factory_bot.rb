01fdbe9d52137a17a304184e2ccbcd900fe2b984
Sasha Grinev <sashagrinev@Elenas-MacBook-Air.local>
2019-01-23 17:04:54 -0500

    add carrierwave functionality to user

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -3,6 +3,7 @@ require 'factory_bot'
 FactoryBot.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
+    avatar { 'default.png' }
     password { 'password' }
     password_confirmation { 'password' }
   end
