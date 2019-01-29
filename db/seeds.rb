3141e13c1d1be37d2dcc322265e180e8b44018d6
Christopher DeLong <christopherdelong415@gmail.com>
2019-01-29 15:27:20 -0500

    css styles added to index and show pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,3 +26,118 @@ Book.create!(
   isbn:"01234567",
   description:"All three of them demonstrate Loyalty and Bravery. Hermione Granger's unique traits are being logical, close-minded, blunt, and book smart. Harry Potter's unique traits are Arrogant, instinctive, obsessive (Hermione is too I suppose), and intimidating (Hermione, again is too, but in a different way.).",
   publication_date: "Jan-01-2001");
+
+Book.create!(
+  name:"To kill a Mocking Bird",
+  author:"Harper Lee",
+  isbn:"37653734747",
+  description: "The story is told by the little six-year-old girl Jean Louise Finch nicknamed Scout. She is a rebellious girl who has tomboy tendencies. The storyline is based in Maycomb, a small town in Alabama in the 1930s where Scout lives with her elder brother Jem, and her father, Atticus, who is widowed.",
+  publication_date: "May-01-1960");
+
+Book.create!(
+  name:"The great Gatsby",
+  author: "The great Gatsby",
+  isbn: "6578986545",
+  description:"The Great Gatsby is told entirely through Nick's eyes; his thoughts and perceptions shape and color the story. Read an in-depth analysis of Nick Carraway. Jay Gatsby - The title character and protagonist of the novel, Gatsby is a fabulously wealthy young man living in a Gothic mansion in West Egg.",
+  publication_date: "April-01-1925");
+
+Book.create!(
+  name:"Catch-22",
+  author: "Joseph Heller",
+  isbn: "23456789",
+  description: "Catch-22 Summary. Captain John Yossarian, a World War II bombardier, is stationed on the island of Pianosa. He is an individualist who seeks to protect his own life by fleeing to the hospital, since a “catch-22” in the Air Force regulations prevents him from being grounded for illness or obtaining a leave.",
+  publication_date: "Nov-01-1961");
+Book.create!(
+  name:"To kill a Mocking Bird",
+  author:"Harper Lee",
+  isbn:"37653734747",
+  description: "The story is told by the little six-year-old girl Jean Louise Finch nicknamed Scout. She is a rebellious girl who has tomboy tendencies. The storyline is based in Maycomb, a small town in Alabama in the 1930s where Scout lives with her elder brother Jem, and her father, Atticus, who is widowed.",
+  publication_date: "May-01-1960");
+Book.create!(
+  name: "War and Peace",
+  author: "Leo Tolstoy",
+  isbn: "12345678",
+  description: "War and Peace broadly focuses on Napoleon's invasion of Russia in 1812 and follows three of the most well-known characters in literature: Pierre Bezukhov, the illegitimate son of a count who is fighting for his inheritance and yearning for spiritual fulfillment; Prince Andrei Bolkonsky, who leaves his family behind",
+  publication_date: "June-01-1865");
+
+Book.create!(
+  name:"On the Road",
+  author: "Joseph Heller",
+  isbn: "23456789",
+  description: "Catch-22 Summary. Captain John Yossarian, a World War II bombardier, is stationed on the island of Pianosa. He is an individualist who seeks to protect his own life by fleeing to the hospital, since a “catch-22” in the Air Force regulations prevents him from being grounded for illness or obtaining a leave.",
+  publication_date: "Nov-01-1961");
+
+Book.create!(
+  name:"Pride and Prejudice",
+  author: "Joseph Heller",
+  isbn: "23456789",
+  description: "Catch-22 Summary. Captain John Yossarian, a World War II bombardier, is stationed on the island of Pianosa. He is an individualist who seeks to protect his own life by fleeing to the hospital, since a “catch-22” in the Air Force regulations prevents him from being grounded for illness or obtaining a leave.",
+  publication_date: "Nov-01-1961");
+
+Book.create!(
+  name:"The Road",
+  author: "Joseph Heller",
+  isbn: "23456789",
+  description: "Catch-22 Summary. Captain John Yossarian, a World War II bombardier, is stationed on the island of Pianosa. He is an individualist who seeks to protect his own life by fleeing to the hospital, since a “catch-22” in the Air Force regulations prevents him from being grounded for illness or obtaining a leave.",
+  publication_date: "Nov-01-1961");
+
+Book.create!(
+  name:"Great Expectations",
+  author: "Joseph Heller",
+  isbn: "23456789",
+  description: "Catch-22 Summary. Captain John Yossarian, a World War II bombardier, is stationed on the island of Pianosa. He is an individualist who seeks to protect his own life by fleeing to the hospital, since a “catch-22” in the Air Force regulations prevents him from being grounded for illness or obtaining a leave.",
+  publication_date: "Nov-01-1961");
+
+Book.create!(
+  name:"Of mice and men",
+  author: "Joseph Heller",
+  isbn: "23456789",
+  description: "Catch-22 Summary. Captain John Yossarian, a World War II bombardier, is stationed on the island of Pianosa. He is an individualist who seeks to protect his own life by fleeing to the hospital, since a “catch-22” in the Air Force regulations prevents him from being grounded for illness or obtaining a leave.",
+  publication_date: "Nov-01-1961");
+
+Book.create!(
+  name:"Ulysses",
+  author: "Joseph Heller",
+  isbn: "23456789",
+  description: "Catch-22 Summary. Captain John Yossarian, a World War II bombardier, is stationed on the island of Pianosa. He is an individualist who seeks to protect his own life by fleeing to the hospital, since a “catch-22” in the Air Force regulations prevents him from being grounded for illness or obtaining a leave.",
+  publication_date: "Nov-01-1961");
+
+Book.create!(
+  name:"Tell me a Riddle",
+  author: "Joseph Heller",
+  isbn: "23456789",
+  description: "Catch-22 Summary. Captain John Yossarian, a World War II bombardier, is stationed on the island of Pianosa. He is an individualist who seeks to protect his own life by fleeing to the hospital, since a “catch-22” in the Air Force regulations prevents him from being grounded for illness or obtaining a leave.",
+  publication_date: "Nov-01-1961");
+
+Book.create!(
+  name:"The Kite Runner",
+  author: "Joseph Heller",
+  isbn: "23456789",
+  description: "Catch-22 Summary. Captain John Yossarian, a World War II bombardier, is stationed on the island of Pianosa. He is an individualist who seeks to protect his own life by fleeing to the hospital, since a “catch-22” in the Air Force regulations prevents him from being grounded for illness or obtaining a leave.",
+  publication_date: "Nov-01-1961");
+
+Book.create!(
+  name:"The little Prince",
+  author: "Joseph Heller",
+  isbn: "23456789",
+  description: "Catch-22 Summary. Captain John Yossarian, a World War II bombardier, is stationed on the island of Pianosa. He is an individualist who seeks to protect his own life by fleeing to the hospital, since a “catch-22” in the Air Force regulations prevents him from being grounded for illness or obtaining a leave.",
+  publication_date: "Nov-01-1961");
+Book.create!(
+  name:"The Color Purple",
+  author: "Joseph Heller",
+  isbn: "23456789",
+  description: "Catch-22 Summary. Captain John Yossarian, a World War II bombardier, is stationed on the island of Pianosa. He is an individualist who seeks to protect his own life by fleeing to the hospital, since a “catch-22” in the Air Force regulations prevents him from being grounded for illness or obtaining a leave.",
+  publication_date: "Nov-01-1961");
+
+Book.create!(
+  name:"Native Son",
+  author: "Joseph Heller",
+  isbn: "23456789",
+  description: "Catch-22 Summary. Captain John Yossarian, a World War II bombardier, is stationed on the island of Pianosa. He is an individualist who seeks to protect his own life by fleeing to the hospital, since a “catch-22” in the Air Force regulations prevents him from being grounded for illness or obtaining a leave.",
+  publication_date: "Nov-01-1961");
+Book.create!(
+  name:"Don Quixote",
+  author: "Joseph Heller",
+  isbn: "23456789",
+  description: "Catch-22 Summary. Captain John Yossarian, a World War II bombardier, is stationed on the island of Pianosa. He is an individualist who seeks to protect his own life by fleeing to the hospital, since a “catch-22” in the Air Force regulations prevents him from being grounded for illness or obtaining a leave.",
+  publication_date: "Nov-01-1961");
